f1c3371effeb2412c95c385c9ff5b51c1f41d15d
Ajala-Marvellous <ajalaoluwamayowa00@gmail.com>
2022-01-19 11:03:08 +0100

    added function to verify if the time is within specified timeframe, not allocated prior and close (30mins) to another preallocated time

--- call-day.py
+++ call-day.py
@@ -32,9 +32,8 @@ class CallDay:
         dates = list of all dates already assigned
         people = list of people on your contact list
         """
-        self.avail_time = None
         self.dates = list()
-        self.people = list()
+        self.people = dict()
 
 def get_time():
     """
@@ -54,6 +53,18 @@ def get_time():
     return starting_hour, ending_hour
 
 def assign_date(starting_hour, ending_hour):
+    """
+    This function takes in the expected starting time and ending time for the calls to be made and generates a random
+    time allocation based on that
+    PARAMETERS:
+
+    INPUT:
+    starting_hour : time frame to start the time allocation from according to user
+    ending_hour : time frame to end the time allocation as specified by te user
+
+    OUTPUT
+    date_assigned : randomly assigned time generated
+    """
     day = random.randint(1,31)
     month = random.randint(1,12)
     year = datetime.datetime.now().year
@@ -61,6 +72,44 @@ def assign_date(starting_hour, ending_hour):
     minute = random.randint(1,60)
     date_assigned = datetime.datetime(year, month, day, hour, minute)
     print(date_assigned)
+
+def verify_date(starting_hour, ending_hour, date_assigned, dates):
+    """
+    This function verifes the random date verified for 3 times:
+    within_time: To verify that the time frame is with the timeframe the user specified
+    not_in list : To verify the date has not been assigned before
+    not_close_to_another : To verify that it is not within 30 minutes range of any other time allocated
+
+    PARAMETERS:
+    INPUT:
+    starting_hour : Time frame from which user wants to start calling
+    ending_hour : Time frame from which user wants calls to end
+    date_assigned : Random date and time allocation created
+    dates: list of all the dates created
+
+    OUTPUT:
+    bool: True/False to ascertain that all parameters have been met
+
+    """
+    within_time = False
+    not_in_list = False
+    not_close_to_another = False
+
+    if date_assigned > starting_hour and date_assigned <= ending_hour:
+        within_time = True
+
+    if date_assigned >= (starting_hour - datetime.timedelta(minutes = 30) and \
+        date_assigned <= (starting_hour + datetime.timedelta(minutes = 30):
+        not_close_to_another = True
+
+    if date_assigned not in dates:
+        not_in_list = True
+
+    if within_time and not_in_list and not_close_to_another:
+        return True
+    else:
+        return False
+
 
 
 def main():
